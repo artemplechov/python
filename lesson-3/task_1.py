82fa478068b813b5a70684e3f7514ee739cad756
artemplechov <artemplechov@gmail.com>
2021-01-26 02:20:59 +0300

    добавил еще 3 задачи

--- lesson-3/task_1.py
+++ lesson-3/task_1.py
@@ -1,1 +1,17 @@
-fdfgsd
+print("Задача 1: Создание функции деления с проверкой деления на 0")
+
+correct = False
+
+def delenie(a,b):
+    s = a / b
+    return s
+
+while correct == False:
+    chislitel = float(input("Введите числитель: "))
+    znamenatel = float(input("Введите знаменатель: "))
+    if znamenatel == 0:
+        print("Внимание! Ошибка! На 0 делить нельзя. Повторите ввод!")
+    else:
+        correct = True
+
+print(f"Результат деления: {delenie(chislitel,znamenatel):.2f}")
