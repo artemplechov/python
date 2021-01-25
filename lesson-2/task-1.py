cfcf28c4243e2cc09ccb57a188921cce624b5d2a
artemplechov <77415016+artemplechov@users.noreply.github.com>
2021-01-25 19:19:05 +0300

    Delete lesson-2 directory

--- lesson-2/task-1.py
+++
@@ -1,8 +0,0 @@
-print("Задание: Создание списка из элементов разных типов и вывод типа каждого элемента")
-my_list = [1,5,44.646,{5, 6, '53BF'},'gfhg',[1,2,'g']]
-length = len(my_list)
-print(f"Мой список: {my_list}")
-print(f"В списке {length} элементов")
-i = 0
-for i in range(length):
-    print(f"{i+1} элемент списка имеет тип {type(my_list[i])}")
--- lesson-2/task-5.py
+++
@@ -1,8 +0,0 @@
-print('Задание: Реализовать структуру "Рейтинг"')
-
-my_list = [7, 5, 3, 3, 2]
-print(f"Текущий рейтинг: {my_list}")
-my_list.append(int(input("Введите новый элемент рейтинга: ")))
-my_list.sort()
-my_list.reverse()
-print(f"Обновленный рейтинг: {my_list}")
