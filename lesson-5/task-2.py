d8cc3a99cbf6d4902c7fbdcf618406b4b8bd6dfd
artemplechov <artemplechov@gmail.com>
2021-02-02 00:30:53 +0300

    добавлена часть решения 2 задачи

--- lesson-5/task-2.py
+++ lesson-5/task-2.py
@@ -2,9 +2,16 @@ print("Задача 2. Создать не программно файл, сох
       "вывести количество строк и количество символов в каждой строке.")
 
 
-my_str = ""
 
-with open("task-2.txt", "r") as f_obj:
+
+with open("task-2.txt", "r", encoding = "utf-8") as f_obj:
+
+    my_str = f_obj.read()
+    print(my_str)
+    f_obj.seek(0)
     for my_str in f_obj:
-        my_str = f_obj.read()
-        print(f"строка - {my_str}")
+        my_str = f_obj.readlines()
+        print(my_str)
+    print(len(my_str))
+    for i in range(len(my_str)):
+        print(len(my_str[i]))
