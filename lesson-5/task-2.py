d3230a44baef8d001726f7622c3ef63b3ecbdda3
artemplechov <artemplechov@gmail.com>
2021-02-02 11:37:26 +0300

    решена 2 задача

--- lesson-5/task-2.py
+++ lesson-5/task-2.py
@@ -1,8 +1,5 @@
 print("Задача 2. Создать не программно файл, сохранить в нем несколько строк, считать эти строки,\n"
-      "вывести количество строк и количество символов в каждой строке.")
-
-
-
+      "вывести количество строк и количество символов в каждой строке.\n")
 
 with open("task-2.txt", "r", encoding = "utf-8") as f_obj:
 
@@ -10,8 +7,9 @@ with open("task-2.txt", "r", encoding = "utf-8") as f_obj:
     print(my_str)
     f_obj.seek(0)
     for my_str in f_obj:
+        f_obj.seek(0)
         my_str = f_obj.readlines()
-        print(my_str)
-    print(len(my_str))
+    print(f"Количество строк в файле: {len(my_str)}")
     for i in range(len(my_str)):
-        print(len(my_str[i]))
+        tmp_str = my_str[i].split()
+        print(f"Количество слов в {i+1} строке: {len(tmp_str)}")
