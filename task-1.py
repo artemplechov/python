a26e608fe35b1951fb5008cebd144da390bb9737
artemplechov <artemplechov@gmail.com>
2021-02-08 19:29:42 +0300

    решена 1 задача

--- task-1.py
+++ task-1.py
@@ -5,53 +5,50 @@ print("Задание 1. Реализовать класс Matrix (матриц�
       "новая матрица.")
 
 class Matrix:
+
     def __init__(self,total_list):
-        self.list_zero = total_list[0]
-        self.list_one = total_list[1]
-        self.list_two = total_list[2]
+        matrix_list = []
+        self.__total_list = total_list
+        for i in range(len(self.__total_list)):
+            for j in range(len(self.__total_list[0])):
+                matrix_list.append(self.__total_list[i][j])
+        self.__matrix_list = matrix_list
+
 
 
     def __str__(self):
         my_str = ''
-        #for i in range(3):
-            #my_str += str(self.list_zero[i])+" "+str(self.list_one[i])+" "+str(self.list_two[i])+" \n"
-        #my_str += str(self.list_zero) + "\n" + str(self.list_one) + "\n" + str(self.list_two)
-        my_str = ("".join(str(self.list_zero)))+"\n"+("".join(str(self.list_one)))+"\n"+("".join(str(self.list_two)))
+        j = 0
+        for i in range(len(self.__matrix_list)):
+            if j !=(len(self.__total_list[0])):
+                my_str += str(self.__matrix_list[i])+" "
+                j += 1
+            else:
+                my_str += "\n"+str(self.__matrix_list[i])+" "
+                j = 1
         return my_str
 
     def __add__(self,other):
-        tmp_str_one = []
-        tmp_str_two = []
         result = []
         my_str = ''
-        for i in range(3):
-            tmp_str_one.append(self.list_zero[i])
-            tmp_str_one.append(self.list_one[i])
-            tmp_str_one.append(self.list_two[i])
-            tmp_str_two.append(other.list_zero[i])
-            tmp_str_two.append(other.list_one[i])
-            tmp_str_two.append(other.list_two[i])
-        for i in range(9):
-            result.append(tmp_str_one[i]+tmp_str_two[i])
-        for i in range(0,9,3):
-            my_str += str(result[i]) + " " + str(result[i+1]) + " " + str(result[i+2]) + " \n"
-        print(tmp_str_one)
-        print(tmp_str_two)
-        print(result)
+        j = 0
+        for i in range(len(self.__matrix_list)):
+            result.append(self.__matrix_list[i]+other.__matrix_list[i])
+            if j !=(len(self.__total_list[0])):
+                my_str += str(result[i])+" "
+                j += 1
+            else:
+                my_str += "\n"+str(result[i])+" "
+                j = 1
         return my_str
 
+my_matrix_one = Matrix (([1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15]))
+my_matrix_two = Matrix (([1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15]))
 
-
-
-
-
-
-my_matrix_one = Matrix (([1,2,3], [4,5,6], [7,8,9]))
-my_matrix_two = Matrix (([1,2,3], [4,5,6], [7,8,9]))
-
-print(my_matrix_one)
-print(my_matrix_two)
-#print(my_matrix.list_one)
-#print(my_matrix.list_two)
+print(f"МАТРИЦА 1\n{my_matrix_one}")
+print(f"МАТРИЦА 2\n{my_matrix_two}")
+#
 z = my_matrix_one+my_matrix_two
-print(z)
+print(f"СУММА ДВУХ МАТРИЦ\n{z}")
+
+
